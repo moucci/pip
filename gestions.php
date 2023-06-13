6d839f6b94b5b64361bcb38f6fa7790b11909da0
Chosess <n.noelle552@gmail.com>
2023-06-13 15:49:15 +0200

    plus de conflit

--- gestions.php
+++ gestions.php
@@ -37,7 +37,22 @@ $sql = "SELECT * FROM `clients` WHERE `id_conseiller` = $id";
 
 $requete = $db->query($sql);
 
+// $requete = $db->prepare($sql);
+
+// $requete->bindValue(":id", $id, PDO::PARAM_INT);
+
+// $requete->execute();
+
 $clients = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+if(empty($clients)){
+    ?>
+
+    <p class="une_alerte_trop_géniale">Vous ne gérez actuellement aucun pig... clients. Veuillez, s'il vous plait, travaillez un minimum !</p>
+
+    <?php
+    
+}
 
 foreach ($clients as $client):
     ?>
@@ -58,9 +73,9 @@ foreach ($clients as $client):
             <div class="trait"></div>
 
             <div>
-                <p class="comptes"><a href="gestions.php?process=Comptes">Compte(s)</a></p>
-                <p class="modifier"><a href="gestions.php?process=edit_client">Modifier</a></p>
-                <p class="supprimer"><a href="gestions.php?process=delete_client">Supprimer</a></p>
+                <p class="comptes"><a href="gestions.php?process=Comptes<?php echo "&id_client=".$client["id"] ?>">Compte(s)</a></p>
+                <p class="modifier"><a href="gestions.php?process=edit_client<?php echo "&id_client=".$client["id"] ?>">Modifier</a></p>
+                <p class="supprimer"><a href="gestions.php?process=delete_client<?php echo "&id_client=".$client["id"] ?>">Supprimer</a></p>
             </div>
 
         </div>
