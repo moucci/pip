e6859738a1ccdb5083d2659d2e33b56a0966c80b
Chosess <n.noelle552@gmail.com>
2023-06-13 13:36:31 +0200

    gestion.php

---
+++ ancien_index.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$title = 'pip page de connexion afficher par le php'
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <title>Pip : Bienvenue</title>
+</head>
+<body>
+        <H1><?php echo $title ?></H1>
+</body>
+</html>
--- gestions.php
+++ gestions.php
@@ -7,17 +7,68 @@ if (!isset($_SESSION->is_connected)) {
     header('Location:connexion.php?is_not_connected');
 }
 
-
+require_once("class/config.php");
 ?>
 <!DOCTYPE html>
 <html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="assets/css/style.css">
+    <!-- <link rel="stylesheet" href="assets/css/style.css"> -->
+    <link rel="stylesheet" href="assets/mscss/css/style.css">
+
     <title>Pip : Bienvenue</title>
 </head>
 <body>
-<H1></H1>
+
+<?php 
+
+$db = getDb();
+
+
+$id = $_SESSION->id;
+
+$sql = "SELECT * FROM `clients` WHERE `id_conseiller` = $id";
+
+
+
+$requete = $db->query($sql);
+
+$clients = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+
+
+foreach($clients as $client):
+    ?>
+    <div class="clients">
+
+        <div class="image">
+            <div></div>
+        </div>
+
+        <div class="infos">
+            <ul>
+                <li>Prénom : <?= $client["prenom"]; ?></li>
+                <li>Nom : <?= $client["nom"]; ?></li>
+                <li>Email : <?= $client["email"]; ?></li>
+                <li>Ville : <?= $client["ville"]; ?></li>
+            </ul>
+
+            <div class="trait"></div>
+
+            <div>
+                <p class="modifier"><a href="gestions.php?process=Comptes">Compte(s)</a></p>
+                <p class="modifier"><a href="gestions.php?process=edit_client">Modifier</a></p>
+                <p class="supprimer"><a href="gestions.php?process=delete_client">Supprimer</a></p>
+            </div>
+
+        </div>
+    </div>
+
+
+<?php endforeach;
+?>
+
+
 </body>
 </html>
