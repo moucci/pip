50e6a83ab9042249babad212de9f65af71534c82
moucci <tamouche.hamid@gmail.com>
2023-06-19 23:31:38 +0200

    add list type compte dynamic

--- add-compte.php
+++ add-compte.php
@@ -1,40 +1,45 @@
 <?php
-session_start();
+    session_start();
 
-//check if user is not connected befor to suscription
-if (!isset($_SESSION['is_connected'])) {
-    header('Location:connexion.php?is_not_connected=add-compte');
-    die();
-}
+    //check if user is not connected befor to suscription
+    if (!isset($_SESSION['is_connected'])) {
+        header('Location:connexion.php?is_not_connected=add-compte');
+        die();
+    }
 
-//check params
-if (empty($_GET['id_client']) || !is_numeric($_GET['id_client'])) {
-    header('Location:gestions.php?process=add-compte-error-not-found');
-    die();
-}
+    //check params
+    if (empty($_GET['id_client']) || !is_numeric($_GET['id_client'])) {
+        header('Location:gestions.php?process=add-compte-error-not-found');
+        die();
+    }
 
-require_once('includes/validator.php');
+    require_once('includes/validator.php');
 
-$db = getDb();
+    $db = getDb();
 
-$q = "SELECT id , nom , prenom FROM clients  WHERE id = :id_client limit 1";
+    $q = "SELECT id , nom , prenom FROM clients  WHERE id = :id_client limit 1";
 
-$req = $db->prepare($q);
-$req->bindParam(':id_client', $_GET['id_client'], PDO::PARAM_INT);
+    $req = $db->prepare($q);
+    $req->bindParam(':id_client', $_GET['id_client'], PDO::PARAM_INT);
 
-//execute query
-try {
-    $req->execute();
-} catch (PDOException $e) {
-    echo "Une erreur est survenue , en temps normal je vous l'affiche pas je la log de un fichier php_error
+    //execute query
+    try {
+        $req->execute();
+    } catch (PDOException $e) {
+        echo "Une erreur est survenue , en temps normal je vous l'affiche pas je la log de un fichier php_error
                  mais la pour le dev voici l'erreur en question :" . $e->getMessage();
-    die;
-}
+        die;
+    }
 
-//fetch data and close connexion
-$client = $req->fetch(PDO::FETCH_OBJ);
-$req->closeCursor();
+    //fetch data and close connexion
+    $client = $req->fetch(PDO::FETCH_OBJ);
+    $req->closeCursor();
 
+    $qTypeCompte = $db->query("SELECT DISTINCT type_compte FROM compte;");
+
+    $listTypeCompte = $qTypeCompte->fetchAll(PDO::FETCH_OBJ);
+
+    $qTypeCompte->closeCursor();
 
 ?>
 <!DOCTYPE html>
@@ -63,14 +68,13 @@ $req->closeCursor();
     </h1>
 </header>
 
-
 <main id="section-add-client">
     <h3 style="color: red"><?= (isset($msgError)) ? $msgError : '' ?></h3>
     <form action="gestion-client.php?process=add_compte" method="post" name="addcompte" class="addcompte">
         <select name="type">
-            <option value="COURANT">Compte Courant</option>
-            <option value="LIVRET-A">livret A</option>
-            <option value="PEL">PEL</option>
+            <?php foreach ($listTypeCompte as $Compte): ?>
+                <option value="<?= $Compte->type_compte ?>"><?= $Compte->type_compte ?></option>
+            <?php endforeach; ?>
         </select>
         <input type="number" name="solde" id="solde" placeholder="Solde a l'ouverture">
         <input type="hidden" name="id_client" value="<?= $client->id ?>">
