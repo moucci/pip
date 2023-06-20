cec5d0b9b82ea31929c465c7084874d9c863c35a
= <=>
2023-06-20 14:01:47 +0200

    update add-compte.php , list enum value on compte table

--- add-compte.php
+++ add-compte.php
@@ -1,45 +1,47 @@
 <?php
-    session_start();
+session_start();
 
-    //check if user is not connected befor to suscription
-    if (!isset($_SESSION['is_connected'])) {
-        header('Location:connexion.php?is_not_connected=add-compte');
-        die();
-    }
+//check if user is not connected befor to suscription
+if (!isset($_SESSION['is_connected'])) {
+    header('Location:connexion.php?is_not_connected=add-compte');
+    die();
+}
 
-    //check params
-    if (empty($_GET['id_client']) || !is_numeric($_GET['id_client'])) {
-        header('Location:gestions.php?process=add-compte-error-not-found');
-        die();
-    }
+//check params
+if (empty($_GET['id_client']) || !is_numeric($_GET['id_client'])) {
+    header('Location:gestions.php?process=add-compte-error-not-found');
+    die();
+}
 
-    require_once('includes/validator.php');
+require_once('includes/validator.php');
 
-    $db = getDb();
+$db = getDb();
 
-    $q = "SELECT id , nom , prenom FROM clients  WHERE id = :id_client limit 1";
+$q = "SELECT id , nom , prenom FROM clients  WHERE id = :id_client limit 1";
 
-    $req = $db->prepare($q);
-    $req->bindParam(':id_client', $_GET['id_client'], PDO::PARAM_INT);
+$req = $db->prepare($q);
+$req->bindParam(':id_client', $_GET['id_client'], PDO::PARAM_INT);
 
-    //execute query
-    try {
-        $req->execute();
-    } catch (PDOException $e) {
-        echo "Une erreur est survenue , en temps normal je vous l'affiche pas je la log de un fichier php_error
+//execute query
+try {
+    $req->execute();
+} catch (PDOException $e) {
+    echo "Une erreur est survenue , en temps normal je vous l'affiche pas je la log de un fichier php_error
                  mais la pour le dev voici l'erreur en question :" . $e->getMessage();
-        die;
-    }
+    die;
+}
 
-    //fetch data and close connexion
-    $client = $req->fetch(PDO::FETCH_OBJ);
-    $req->closeCursor();
+//fetch data and close connexion
+$client = $req->fetch(PDO::FETCH_OBJ);
+$req->closeCursor();
 
-    $qTypeCompte = $db->query("SELECT DISTINCT type_compte FROM compte;");
+//query to get enum  type_compte
+$qTypeCompte = "SELECT COLUMN_TYPE FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = 'compte' AND COLUMN_NAME = 'type_compte'";
+$reqTypeCompte = $db->query($qTypeCompte);
+$enumList = $reqTypeCompte->fetch(PDO::FETCH_OBJ);
 
-    $listTypeCompte = $qTypeCompte->fetchAll(PDO::FETCH_OBJ);
-
-    $qTypeCompte->closeCursor();
+//extract  value
+$listTypeCompte = explode(",", str_replace("'", "", substr($enumList->COLUMN_TYPE, 5, (strlen($enumList->COLUMN_TYPE) - 6))));
 
 ?>
 <!DOCTYPE html>
@@ -72,8 +74,8 @@
     <h3 style="color: red"><?= (isset($msgError)) ? $msgError : '' ?></h3>
     <form action="gestion-client.php?process=add_compte" method="post" name="addcompte" class="addcompte">
         <select name="type">
-            <?php foreach ($listTypeCompte as $Compte): ?>
-                <option value="<?= $Compte->type_compte ?>"><?= $Compte->type_compte ?></option>
+            <?php foreach ($listTypeCompte as $typeCompte): ?>
+                <option value="<?= $typeCompte ?>"><?= $typeCompte ?></option>
             <?php endforeach; ?>
         </select>
         <input type="number" name="solde" id="solde" placeholder="Solde a l'ouverture">
